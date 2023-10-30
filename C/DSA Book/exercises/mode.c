fd1fe2e0c05e9c4dfc7e02c54da2fd71949f0499
Codewani <kondwanimwape013@gmail.com>
2023-10-30 18:32:38 +0200

    change mode

--- C/DSA Book/exercises/mode.c
+++ C/DSA Book/exercises/mode.c
@@ -4,8 +4,8 @@ int mode(int array[], int len);
 
 int main(void)
 {
-    int numbers[] = {1, 2, 3, 3};
-    printf("mode: %i", mode(numbers, 4));
+    int numbers[] = {1, 2, 3, 4, 4};
+    printf("mode: %i", mode(numbers, 5));
 }
 /*
  *return: -1 when there is no mode, and mode when there is one
@@ -13,11 +13,13 @@ int main(void)
 int mode(int array[], int len)
 {
     int i, mode;
-    int temp = 0;
-    int count_mult_modes = 0;
+    /*highest_freq keeps track of the the frequency of the current mode*/
+    int highest_freq = 0;
+    /*in a situation where a number with the same frequency as the current mode has been found, second_highest_freq stores that number*/
+    int second_highest_freq = 0;
     for (i = 0; i < len; i++)
     {
-        if (i > 0 && array[i] == mode)
+        if (array[i] == mode)
             continue;
         int count = 0;
         for (int j = i + 1; j < len; j++)
@@ -27,17 +29,17 @@ int mode(int array[], int len)
                 count++;
             }
         }
-        if (count > temp)
+        if (count == highest_freq)
+        {
+            second_highest_freq = highest_freq;
+        }
+        else if (count > highest_freq)
         {
             mode = array[i];
-            temp = count;
-        }
-        if (count == mode)
-        {
-            count_mult_modes++;
+            highest_freq = count;
         }
     }
-    if (count_mult_modes > 1 || temp == 0)
+    if (highest_freq == second_highest_freq)
     {
         return(-1);
     }
